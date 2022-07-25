683320671ec24dbe3ef302dbbb2f1e8e71b9910f
Ryukawastaken <54033519+Ryukawastaken@users.noreply.github.com>
2022-07-25 12:45:46 +1000

    Week 01 Lab Tasks Completed
    Setup Visual Studio with basic ASP.Net MVC Application

---
+++ FIT5032_MyFirstWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FIT5032_MyFirstWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "This is a changed application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "This is a changed contact page.";
+
+            return View();
+        }
+    }
+}
