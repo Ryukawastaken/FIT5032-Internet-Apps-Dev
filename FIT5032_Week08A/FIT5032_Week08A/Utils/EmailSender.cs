d5a0a9f3340e2c13418453e343b86ce996ee4c9d
Nic Pallant <nic.pallant@monash.edu>
2022-09-18 16:23:11 +1000

    Finished Studio Task 8.1
    Finished Studio Task 8.1 by sending email with Sendgrid, still need to write up document

---
+++ FIT5032_Week08A/FIT5032_Week08A/Utils/EmailSender.cs
@@ -0,0 +1,28 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FIT5032_Week08A.Utils
+{
+    public class EmailSender
+    {
+        // Please use your API KEY here.
+        private const String API_KEY = "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";
+
+        public void Send(String toEmail, String subject, String contents)
+        {
+            var client = new SendGridClient(API_KEY);
+            var from = new EmailAddress("npal0002@student.monash.edu", "Nicolas Pallant");
+            var to = new EmailAddress(toEmail, "");
+            var plainTextContent = contents;
+            var htmlContent = "<p>" + contents + "</p>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = client.SendEmailAsync(msg);
+        }
+
+    }
+}
